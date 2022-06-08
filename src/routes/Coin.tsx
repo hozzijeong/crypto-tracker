760d5de23216cbb594193fcdc874ae86608f4a9d
hozzijeong <ghwls0905@naver.com>
2022-06-09 01:58:33 +0900

    Design: Global 디자인 설정

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,17 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 type RouteParams = {
 	coinId: string;
 };
 
+const Title = styled.h1`
+	color: ${(props) => props.theme.accentColor};
+`;
+
 function Coin() {
 	const { coinId } = useParams<RouteParams>();
 
-	return <h1>Coin:{coinId}</h1>;
+	return <Title>{coinId}</Title>;
 }
 export default Coin;
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const theme: DefaultTheme = {
+	bgColor: '#2f3640',
+	textColor: '#f5f6fa',
+	accentColor: '#4cd137',
+};
