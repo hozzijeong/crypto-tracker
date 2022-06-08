578f7fb3c36f197b639b131ae2e2075e33f94151
hozzijeong <ghwls0905@naver.com>
2022-06-08 22:38:16 +0900

    Refactor: Route 설정 및 eslint 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import Router from './Router';
 
 function App() {
-	return <div>HelloWorld</div>;
+	return <Router />;
 }
 
 export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Coin from './routes/Coin';
+import Coins from './routes/Coins';
+
+function Router() {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/:coin-id">
+					<Coins />
+				</Route>
+				<Route path="/">
+					<Coin />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
+}
+
+export default Router;
