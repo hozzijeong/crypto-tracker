ffdd110ba0da2f3e73dc06444bfdbeefc7965794
hozzijeong <ghwls0905@naver.com>
2022-06-11 03:24:16 +0900

    Refactor: router 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -11,11 +11,11 @@ function Router() {
 			<div>
 				<Header />
 				<Routes>
-					<Route path="/:coinId" element={<Coin />}>
-						<Route path="chart" element={<Chart />} />
-						<Route path="price" element={<Price />} />
+					<Route path={`${process.env.PUBLIC_URL}/:coinId`} element={<Coin />}>
+						<Route path={`${process.env.PUBLIC_URL}/chart`} element={<Chart />} />
+						<Route path={`${process.env.PUBLIC_URL}/price`} element={<Price />} />
 					</Route>
-					<Route path="/" element={<Coins />} />
+					<Route path={`${process.env.PUBLIC_URL}/`} element={<Coins />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
